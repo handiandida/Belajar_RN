0ff6b32e9e77a8a3e2e2a971ca847802a44d4059
unknown <handiandida@gmail.com>
2022-07-07 11:23:28 +0700

    building user authentication app with firebase

---
+++ userAuthentication/src/components/ui/IconButton.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Icons from 'react-native-vector-icons/Ionicons';
+import { Pressable, StyleSheet } from 'react-native';
+
+function IconButton({ icon, color, size, onPress }) {
+  return (
+    <Pressable
+      style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+      onPress={onPress}
+    >
+      <Icons name={icon} color={color} size={size} />
+    </Pressable>
+  );
+}
+
+export default IconButton;
+
+const styles = StyleSheet.create({
+  button: {
+    margin: 8,
+    borderRadius: 20,
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+});
+
