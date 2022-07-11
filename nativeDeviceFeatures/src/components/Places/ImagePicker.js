9980119b5594e69cd2dd31329d86c12b0f4a2084
unknown <handiandida@gmail.com>
2022-07-11 14:37:00 +0700

    fixing bug camera permission in android

--- nativeDeviceFeatures/src/components/Places/ImagePicker.js
+++ nativeDeviceFeatures/src/components/Places/ImagePicker.js
@@ -1,24 +1,67 @@
-import {Button, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  PermissionsAndroid,
+  Image,
+} from 'react-native';
+import React, {useState} from 'react';
 import {launchCamera} from 'react-native-image-picker';
+import {Colors} from '../../constants/colors';
+import OutlinedButton from '../UI/OutlinedButton';
 
 export default function ImagePicker() {
-  async function takeImagehandler() {
-    const image = await launchCamera({
-      //   allowsEditing: true,
-      //   aspect: [16, 9],
-      quality: 0.5,
-      saveToPhotos: true,
+  const [pickedImage, setPickedImage] = useState(null);
+
+  const takeImagehandler = () => {
+    const option = {
+      mediaType: 'photo',
+      quality: 1,
+    };
+
+    launchCamera(option, res => {
+      if (res.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (res.errorCode) {
+        console.log(res.errorMessage);
+      } else {
+        const data = res.assets[0];
+        setPickedImage(data);
+        console.log(data);
+      }
     });
-    console.log(image);
-  }
+  };
+
+  let imagePreview = <Text>No image taken yet.</Text>;
+
+  {pickedImage != null && (
+    imagePreview = <Image
+      source={{uri: pickedImage.uri}}
+      style={styles.image}
+    />
+  )}
 
   return (
     <View>
-      <View></View>
-      <Button title="Take Image" onPress={takeImagehandler} />
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <OutlinedButton icon="camera" onPress={takeImagehandler}>Take Image</OutlinedButton>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: '100%',
+    height: 200,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: '100%',
+    height: '100%'
+  }
+});
