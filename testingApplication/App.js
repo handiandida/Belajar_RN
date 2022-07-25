3502d75951e554e1425ccbc6fb44d8db84f5460f
Dida Handian <dida.handian@wgs-id.com>
2022-07-25 11:02:16 +0700

    test terbaru

---
+++ testingApplication/App.js
@@ -0,0 +1,26 @@
+import { Button, Linking, StyleSheet, Text, View } from 'react-native'
+import React, { Component } from 'react'
+import AndroidOpenSettings from 'react-native-android-open-settings'
+
+export class App extends Component {
+
+  handler () {
+    Linking.openSettings()
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button title='Test App' onPress={this.handler} />
+        <Button title='Coba' onPress={() => Linking.openSettings() }/>
+        <Button title='opensetting' onPress={() => AndroidOpenSettings.appDetailsSettings() }/>
+      </View>
+    )
+  }
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+});
