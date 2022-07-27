3c9ec154bcbfcd541a9720e639eea4376d9a9d2e
Dida Handian <dida.handian@wgs-id.com>
2022-07-27 16:04:18 +0700

    building e-laundry app

---
+++ antarLaundry/src/assets/images/index.js
@@ -0,0 +1,4 @@
+import Logo from './logo.png'
+import SplashBackground from './splashBackground.png'
+
+export {Logo, SplashBackground}
---
+++ antarLaundry/src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "./home";
+import Akun from "./akun";
+import Splash from "./splash";
+import Pesanan from "./pesanan";
+
+export {Home, Akun, Splash, Pesanan}
